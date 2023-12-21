af667b12f80bacbaf5e0d26b9ab6749c6e34e33e
mhmjalali <muhammad.art79@gmail.com>
2023-12-21 04:51:02 -0800

    responsive part of Footer Completed

--- src/components/lead-generation/Footer/index.jsx
+++ src/components/lead-generation/Footer/index.jsx
@@ -7,6 +7,10 @@ const FooterBox = styled.section`
     height: 520px;
     margin-top: 100px;
     position: relative;
+    @media only screen and (max-width: 768px) {
+        height: 330px;
+        margin-top: unset;
+    }
 `;
 
 const Footer = () => {
